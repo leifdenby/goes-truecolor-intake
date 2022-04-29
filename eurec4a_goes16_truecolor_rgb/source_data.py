1367e027bbb490d838472aad603b2023b8597f8b
Leif Denby <leif@denby.eu>
2022-04-29 10:19:09 +0100

    first rudimentary pipeline

--- eurec4a_goes16_truecolor_rgb/source_data.py
+++ eurec4a_goes16_truecolor_rgb/source_data.py
@@ -10,11 +10,12 @@ import matplotlib.pyplot as plt
 import cartopy.crs as ccrs
 import xarray as xr
 import rioxarray as rxr
-from satpy.writers import get_enhanced_image
+from loguru import logger
+from tqdm.auto import tqdm
+from suntime import Sun
 
 
-from suntime import Sun, SunTimeException
-import datetime
+DEBUG = True
 
 
 DOMAIN_BBOX = [-60, -50, 11, 16]  # WESN
@@ -49,7 +50,7 @@ def download_daytime_radiance_source_files(
     t_center = t_min + dt_daytime / 2
 
     cli = satdata.Goes16AWS()
-    filenames = []
+    files_per_channel = {}
     for channel in [1, 2, 3]:
         keys = cli.query(
             # XXX
@@ -59,18 +60,65 @@ def download_daytime_radiance_source_files(
             dt_max=dt_daytime / 2,
             channel=channel,
         )
-        fn = cli.download(keys)
-        filenames.append(fn)
+        if DEBUG:
+            keys = keys[:2]
+        filenames = cli.download(keys)
+        files_per_channel[channel] = filenames
 
-    return filenames
+    return files_per_channel
+
+
+def merge_multichannel_sources(files_per_channel, time_fn):
+    channel_files_by_timestamp = {}
+    N_channels = len(files_per_channel)
+    for channel, channel_files in files_per_channel.items():
+        for ch_filename in channel_files:
+            file_timestamp = time_fn(filename=ch_filename)
+            time_group = channel_files_by_timestamp.setdefault(file_timestamp, {})
+            time_group[channel] = ch_filename
+
+    scene_filesets = []
+
+    for timestamp in sorted(channel_files_by_timestamp.keys()):
+        timestamp_files = channel_files_by_timestamp[timestamp]
+
+        if len(timestamp_files) == N_channels:
+            scene_filesets.append(
+                [timestamp_files[channel] for channel in files_per_channel.keys()]
+            )
+        else:
+            logger.warn(
+                f"Only {len(timestamp_files)} were found for timestamp {timestamp}"
+                " so this timestamp will be excluded"
+            )
+
+    return scene_filesets
 
 
 def main():
-    date = datetime.datetime.now()
+    date = datetime.datetime(year=2020, month=1, day=1, hour=10, minute=0)
     print(date)
     bbox = DOMAIN_BBOX
-    files = download_daytime_radiance_source_files(date=date, bbox=bbox)
-    print(files)
+    files_per_channel = download_daytime_radiance_source_files(date=date, bbox=bbox)
+
+    def parse_time_from_source_filename(filename):
+        return satdata.Goes16AWS.parse_key(filename, parse_times=True)["start_time"]
+
+    scene_filesets = merge_multichannel_sources(
+        files_per_channel, time_fn=parse_time_from_source_filename
+    )
+    for scene_files in tqdm(scene_filesets):
+        start_time = parse_time_from_source_filename(scene_files[0])
+        start_time_str = start_time.isoformat().replace("-", "").replace(":", "") + "Z"
+        geotiff_filepath = Path(f"{start_time_str}.tif")
+        if not geotiff_filepath.exists():
+            create_cropped_truecolor_geotiff(
+                scene_filepaths=scene_files, geotiff_filepath=geotiff_filepath, bbox=bbox
+            )
+
+        geotiff_to_netcdf(geotiff_filepath=geotiff_filepath, channel1_filepath=scene_files[0])
+
+    print(scene_filesets)
     # scene_meta = satdata.Goes16AWS.parse_key(scene_filepaths[0], parse_times=True)
     # start_time = scene_meta["start_time"]
 
@@ -105,24 +153,40 @@ def create_cropped_truecolor_geotiff(scene_filepaths, geotiff_filepath, bbox):
     new_scn.save_dataset("true_color", writer="geotiff", filename=geotiff_filepath)
 
 
-def geotiff(geotiff_filepath):
-    """ """
+def geotiff_to_netcdf(geotiff_filepath, channel1_filepath):
+    """
+    Convert geotiff image in filepath `geotiff_filepath` to a netCDF file that
+    can be plotted with `matplotlib.imshow` which contains projection
+    information in a CF-compliant manner (so that it can be read by `metpy`).
+
+    NB: Because of the incompatibility between cartopy and pyproj at the moment we
+    can't read the projection info directly from the geotiff, but instead must
+    get it from one of the radiance channel source files. This this projection
+    information will be read from `channel1_filepath`
+
+    """
     # Convert geotiff into netCDF file with RGB image data
-    ds_ch1 = xr.open_dataset(filenames[0])
+    ds_ch1 = xr.open_dataset(channel1_filepath)
 
     # read tiff with rioxarray
-    da = rxr.open_rasterio(filename)
+    da = rxr.open_rasterio(geotiff_filepath)
 
-    # remove attribute that we can't easily serialise
-    da2 = da.copy().drop("spatial_ref").expand_dims("t") / 255
+    # remove attribute that we can't easily serialise and scale from uint8 in
+    # range 0..255 to float in range 0...1
+    da_img_floats = da.copy().drop("spatial_ref").expand_dims("t") / 255
 
     # copy over projection info in a CF-compliant manner so that metpy can parse it
     grid_mapping = ds_ch1.Rad.grid_mapping
-    da2.coords[grid_mapping] = ds_ch1[grid_mapping].drop(["t", "x_image", "y_image"])
-    da2.attrs["grid_mapping"] = grid_mapping
-    da2.name = "true_color"
+    da_img_floats.coords[grid_mapping] = ds_ch1[grid_mapping].drop(
+        ["t", "x_image", "y_image"]
+    )
+    da_img_floats.attrs["grid_mapping"] = grid_mapping
+    da_img_floats.name = "true_color"
 
-    da2.to_netcdf(filename.replace(".tif", ".nc"))
+    netcdf_filepath = geotiff_filepath.parent / geotiff_filepath.name.replace(
+        ".tif", ".nc"
+    )
+    da_img_floats.to_netcdf(netcdf_filepath)
 
 
 if __name__ == "__main__":

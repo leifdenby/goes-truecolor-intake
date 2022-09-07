162346e9e4e93db0f761eeee871bc787eb0f8ce5
leif <leif@denby.eu>
2022-09-07 13:56:15 +0000

    working local pipeline

--- goes-truecolor-intake/__main__.py
+++ goes-truecolor-intake/__main__.py
@@ -2,7 +2,7 @@
 # coding: utf-8
 import isodate
 
-from .s3_upload import create_catalog
+from .s3_upload import create_catalog, upload_files
 from .source_data import calc_minmax_time_for_bbox, create_truecolor_scene_files
 
 
@@ -28,17 +28,46 @@ def main():
         type=_bbox_arg,
         help="Domain bounding-box W,E,S,N for example -60,-50,11,16",
     )
+    argparser.add_argument("--local-path", default=".")
+    argparser.add_argument("--s3-access-key", default=None)
+    argparser.add_argument("--s3-secret-key", default=None)
+    argparser.add_argument("--s3-bucket-name", default=None)
+    argparser.add_argument("--target-hostname", default="local")
     args = argparser.parse_args()
+
+    if args.target_hostname != "local":
+        reqd_args = "s3_access_key s3_secret_key s3_bucket_name".split()
+        if any(getattr(args, v) is None for v in reqd_args):
+            raise Exception(
+                "To upload to an S3 host you need to provide: " + ", ".join(reqd_args)
+            )
+
+    local_path = args.local_path
+    name = "goesrgb"
 
     bbox = args.bbox
     date = args.date
     t_min, t_max = calc_minmax_time_for_bbox(date=date, bbox=bbox)
 
-    create_truecolor_scene_files(t_min=t_min, t_max=t_max, bbox=bbox)
+    create_truecolor_scene_files(
+        t_min=t_min, t_max=t_max, bbox=bbox, data_path=local_path
+    )
 
     create_catalog(
-        name="goesrgb", host_url="minio.denby.eu", remote_path="eurec4a-environment"
+        catalog_path=local_path,
+        name=name,
+        host_url=args.target_hostname,
+        remote_path=args.s3_bucket_name,
     )
+
+    if args.target_hostname != "local":
+        upload_files(
+            local_path=local_path,
+            host_url=args.target_hostname,
+            remote_path=args.s3_bucket_name,
+            s3_access_key=args.s3_access_key,
+            s3_secret_key=args.s3_secret_key,
+        )
 
 
 if __name__ == "__main__":
--- goes-truecolor-intake/s3_upload.py
+++ goes-truecolor-intake/s3_upload.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 # coding: utf-8
 
+from pathlib import Path
+
 import requests
 import s3fs
 import yaml
@@ -16,46 +18,64 @@ LOCAL_CATALOG_FILENAME = "catalog.yml"
 BACKEND_SLEEP_TIME = 10
 
 
-def _upload(ds, host_url, remote_path, auth_info):
+def upload_files(local_path, host_url, remote_path, s3_access_key, s3_secret_key):
     logger.info(f"storing in {host_url} {remote_path}...", end="", flush=True)
-    s3_access_key = auth_info["s3_access_key"]
-    s3_secret_key = auth_info["s3_secret_key"]
+
+    endpoint_url = f"https://{host_url}"
+    logger.info(f"Uploading files to remote S3 bucket {endpoint_url}")
 
     fs = s3fs.S3FileSystem(
         anon=False,
         key=s3_access_key,
         secret=s3_secret_key,
-        client_kwargs={"endpoint_url": host_url},
+        client_kwargs={"endpoint_url": endpoint_url},
     )
 
-    mapper = fs.get_mapper(remote_path)
-    ds.to_zarr(mapper, consolidated=True)
+    files_to_copy = list(Path(local_path).glob("*.tif"))
+
+    logger.info(fs.ls("eurec4a-environment"))
+
+    for fp_local in files_to_copy:
+        fs.put(str(fp_local), str(Path("eurec4a-environment/goesrgb") / fp_local.name))
 
 
-def create_catalog(name, host_url, remote_path):
+def create_catalog(catalog_path, name, host_url, remote_path):
     cat = {}
     cat["sources"] = {}
+
+    if host_url == "local":
+        url_root = "{{CATALOG_DIR}}"
+    else:
+        url_root = f"s3://{remote_path}/{name}"
 
     cat_entry = {
         "description": "Truecolor RGB of GOES-16",
         "driver": "rasterio",
         "args": {
-            # "urlpath": f"simplecache::s3://{remote_path}/{name}/\{{DATETIME_FORMAT:time}\}.tif",
-            "urlpath": "{{CATALOG_DIR}}/{time:" + DATETIME_FORMAT + "}.tif",
-            "storage_options": {
-                "s3": {
-                    "anon": True,
-                    "client_kwargs": {"endpoint_url": host_url},  # noqa
-                }
-            },
+            "urlpath": url_root + "/{time:" + DATETIME_FORMAT + "}.tif",
             "engine": "rioxarray",
             "concat_dim": "time",
             "chunks": {"band": 1, "x": 100, "y": 100},
         },
     }
+
+    if host_url != "local":
+        endpoint_url = f"https://{host_url}"
+        cat_entry["args"]["storage_options"] = {
+            "anon": True,
+            "client_kwargs": {"endpoint_url": endpoint_url},
+        }
+        cat_entry["cache"] = [
+            {
+                "argkey": "urlpath",
+                "regex": "goesrgb",
+                "type": "file",
+            }
+        ]
+
     cat["sources"][name] = cat_entry
 
-    with open(LOCAL_CATALOG_FILENAME, "w") as fh:
+    with open(Path(catalog_path) / LOCAL_CATALOG_FILENAME, "w") as fh:
         fh.write(yaml.dump(cat, default_flow_style=None))
 
     logger.info(f"Wrote intake catalog file to `{LOCAL_CATALOG_FILENAME}`")
